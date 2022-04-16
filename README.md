b428c0e3c4fd8bfdbfe8338a8346be3c4e0a7c55
prongbang <prongbang@gmail.com>
2022-04-17 00:06:05 +0700

    initial

---
+++ README.md
@@ -0,0 +1,35 @@
+# warp-ws
+
+Learning by doing from [https://github.com/zupzup/warp-websockets-example](https://github.com/zupzup/warp-websockets-example)
+
+### Register
+
+```shell
+curl -X POST 'http://localhost:8000/register' -H 'Content-Type: application/json' -d '{ "user_id": 1 }'
+```
+
+Response
+
+```json
+{"url":"ws://127.0.0.1:8000/ws/e1fadcd3919a4b78aad0251eed908b70"}
+```
+
+### Unregister
+
+```shell
+curl -X DELETE 'http://localhost:8000/register/e1fadcd3919a4b78aad0251eed908b70'
+```
+
+### Connect to the WebSocket
+
+```shell
+ws://127.0.0.1:8000/ws/e1fadcd3919a4b78aad0251eed908b70
+```
+
+### Publish Messages
+
+```shell
+curl -X POST 'http://localhost:8000/publish' \
+    -H 'Content-Type: application/json' \
+    -d '{"user_id": 1, "topic": "cats", "message": "are awesome"}'
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+mod ws;
+mod handler;
+mod model;
+
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::{RwLock};
+use warp::{Filter};
+use crate::model::Clients;
+
+#[tokio::main]
+async fn main() {
+    let clients: Clients = Arc::new(RwLock::new(HashMap::new()));
+
+    let health_route = warp::path!("health").and_then(handler::health_handler);
+
+    let register = warp::path("register");
+    let register_routes = register
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(ws::with_clients(clients.clone()))
+        .and_then(handler::register_handler)
+        .or(register
+            .and(warp::delete())
+            .and(warp::path::param())
+            .and(ws::with_clients(clients.clone()))
+            .and_then(handler::unregister_handler));
+
+    let publish = warp::path!("publish")
+        .and(warp::body::json())
+        .and(ws::with_clients(clients.clone()))
+        .and_then(handler::publish_handler);
+
+    let ws_route = warp::path("ws")
+        .and(warp::ws())
+        .and(warp::path::param())
+        .and(ws::with_clients(clients.clone()))
+        .and_then(handler::ws_handler);
+
+    let routes = health_route
+        .or(register_routes)
+        .or(ws_route)
+        .or(publish)
+        .with(warp::cors().allow_any_origin());
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
